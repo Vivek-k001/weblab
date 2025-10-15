01fd03fea57a137b6dfae7ffab04a416c6374d19
Vivek-k001 <vk3699010@gmail.com>
2025-10-15 16:19:48 +0530

    eg

---
+++ rec12button/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("colorBtn");
+
+button.addEventListener("click",function() {
+document.body.style.backgroundColor = "red";
+});
+button.addEventListener("dbclick",function() {
+document.body.style.backgroundColor = "green";
+});

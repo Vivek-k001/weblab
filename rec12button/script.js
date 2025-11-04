6a0a4d825f4861f6fae3af5dfeacf432379061c8
Vivek-k001 <vk3699010@gmail.com>
2025-11-04 12:20:35 +0530

    rhE

--- rec12button/script.js
+++
@@ -1,7 +0,0 @@
-let bgcolor = document.getElementById('change');
-bgcolor.addEventListener('click', function() {
-document.body.style.backgroundColor = "red";
-});
-bgcolor.addEventListener('dblclick', function() {
-document.body.style.backgroundColor = "green";
-});
